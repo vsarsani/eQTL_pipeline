63bdb2b2013caa6424f91b8c68659c21edb69112
kzuckerman <kalebzuckerman@gmail.com>
2025-12-17 10:08:21 -0500

    RAM optimized expr_comp_matrices

--- expr_comp_matrices.py
+++ expr_comp_matrices.py
@@ -5,7 +5,6 @@ import pandas as pd
 import decoupler as dc
 from sccoda.util import cell_composition_data as dat
 
-sc.settings.n_jobs = 8
 input_files = [f for f in sys.argv[2:] if f[-5:] == ".h5ad"]
 cluster_names = [f for f in sys.argv[2:] if f[-5:] != ".h5ad"]
 
@@ -24,17 +23,7 @@ for file, cluster in zip(input_files, cluster_names):
         cell_type_identifier=cluster,
         sample_identifier="Sample"
     )
-    # print(data_scanpy_1)
 
-    combined_df_corrected =pd.concat([data_scanpy_1.obs, pd.DataFrame(data_scanpy_1.X, index=data_scanpy_1.obs.index, columns=data_scanpy_1.var.index)],axis=1)
-    if "leiden" in cluster:
-        new_column_names = ['cluster' + str(col) for col in data_scanpy_1.var.index]
-    else:
-        new_column_names = list(data_scanpy_1.var.index)
-    # Update the relevant columns in combined_df_corrected with the new names
-    combined_df_corrected.columns = list(data_scanpy_1.obs.columns) + new_column_names
-    
-    
     # Pseudobulk aggregation
     pdata = dc.pp.pseudobulk(
         adata,
@@ -44,6 +33,7 @@ for file, cluster in zip(input_files, cluster_names):
         mode="sum",
         empty=True
     )
+    del adata
     dc.pp.filter_samples(pdata, min_cells=10, min_counts=100)
     
     # Normalize total counts and log-transform
@@ -58,17 +48,28 @@ for file, cluster in zip(input_files, cluster_names):
     sc.pp.scale(pdata.copy(), max_value=10)
     
     # Save the processed data as a CSV file
-    data=pd.DataFrame(pdata.X, index=pdata.obs_names, columns=pdata.var_names)
     output_file1 = os.path.join(f"{workdir}/processed_matrices/{os.path.splitext(os.path.basename(file))[0]}_expression_matrix_ds.csv")
     output_file2 = os.path.join(f"{workdir}/processed_matrices/{os.path.splitext(os.path.basename(file))[0]}_composition_matrix_ds.csv")
-    common_samples = data.index.intersection(combined_df_corrected.index)
+
     # Filter both datasets to only include common samples
+    common_samples = data.index.intersection(data_scanpy_1.obs.index)
+
+    data = pd.DataFrame(pdata.X, index=pdata.obs_names, columns=pdata.var_names)
+    del pdata
     data_aligned = data.loc[common_samples]
-    combined_df_corrected_aligned = combined_df_corrected.loc[common_samples]
-    
     data_aligned.to_csv(output_file1)
-    combined_df_corrected_aligned.to_csv(output_file2)
-    
     print(f"Saved {output_file1}")
+    del data, data_aligned
+    
+    combined_df_corrected =pd.concat([data_scanpy_1.obs, pd.DataFrame(data_scanpy_1.X, index=data_scanpy_1.obs.index, columns=data_scanpy_1.var.index)],axis=1)
+    if "leiden" in cluster:
+        new_column_names = ['cluster' + str(col) for col in data_scanpy_1.var.index]
+    else:
+        new_column_names = list(data_scanpy_1.var.index)
+    # Update the relevant columns in combined_df_corrected with the new names
+    combined_df_corrected.columns = list(data_scanpy_1.obs.columns) + new_column_names
+
+    combined_df_corrected_aligned = combined_df_corrected.loc[common_samples]
+    combined_df_corrected_aligned.to_csv(output_file2)
     print(f"Saved {output_file2}")
 print("Processing completed.")
